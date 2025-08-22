b0f21dce86e6f606a2f31565fe0b135569f4eafe
onyedkichi23 <ozohenrywk@gmail.com>
2025-08-22 07:03:46 +0100

    refactor(login): Revert to a pending state

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,7 +1,21 @@
 /** @format */
 
+import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
+import { Link } from "expo-router";
+import Animated, { FadeInLeft, FadeOutRight } from "react-native-reanimated";
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function LoginScreen() {
-	return <Text>Login</Text>;
+	return (
+		<AnimatedBox
+			className="flex-1 items-center justify-center"
+			entering={FadeInLeft}
+			exiting={FadeOutRight}>
+			<Text>Login</Text>
+
+			<Link href={"/signup"}>Go to signup</Link>
+		</AnimatedBox>
+	);
 }
