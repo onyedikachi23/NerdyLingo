a16f7cd7712a55a7277df0e6934e5b8b18b64f3f
onyedkichi23 <ozohenrywk@gmail.com>
2025-08-23 07:34:17 +0100

    refactor(signup layout): Remove root scrollview and add white background for the logo to match design.

--- src/app/(auth)/signup/_layout.tsx
+++ src/app/(auth)/signup/_layout.tsx
@@ -3,7 +3,6 @@
 import { Box } from "@/components/ui/box";
 import { Image } from "@/components/ui/image";
 import { Stack } from "expo-router";
-import { ScrollView } from "react-native";
 import Animated, { FadeInLeft, FadeOutRight } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -15,10 +14,8 @@ export default function SignupScreen() {
 			className="flex-1"
 			entering={FadeInLeft}
 			exiting={FadeOutRight}>
-			<SafeAreaView className="flex-1">
-				<ScrollView
-					className="flex-1"
-					contentContainerClassName="gap-12 p-4 flex-1">
+			<SafeAreaView className="flex-1 gap-4 p-4">
+				<Box className="w-full items-center justify-center bg-white p-4">
 					<Image
 						// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 						source={require("@/assets/images/brand/logo.svg")}
@@ -27,21 +24,21 @@ export default function SignupScreen() {
 						className={"mx-auto"}
 						size={"2xl"}
 					/>
+				</Box>
 
-					<Stack
-						screenOptions={{
-							headerShown: false,
-						}}>
-						<Stack.Screen
-							name="index"
-							options={{ title: "Sign up options" }}
-						/>
-						<Stack.Screen
-							name="details"
-							options={{ title: "Sign up with Email" }}
-						/>
-					</Stack>
-				</ScrollView>
+				<Stack
+					screenOptions={{
+						headerShown: false,
+					}}>
+					<Stack.Screen
+						name="index"
+						options={{ title: "Sign up options" }}
+					/>
+					<Stack.Screen
+						name="email"
+						options={{ title: "Sign up with Email" }}
+					/>
+				</Stack>
 			</SafeAreaView>
 		</AnimatedBox>
 	);
