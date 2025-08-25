98b64c95479e79135d362e68aee404bf49314132
onyedkichi23 <ozohenrywk@gmail.com>
2025-08-25 06:54:00 +0100

    feat(email-signup): integrate form validation with `@tanstack/react-form`

--- src/app/(auth)/signup/email.tsx
+++ src/app/(auth)/signup/email.tsx
@@ -19,11 +19,68 @@ import { Input, InputField } from "@/components/ui/input";
 import { AlertCircleIcon, ChevronRight } from "lucide-react-native";
 import { ScrollView } from "react-native";
 import Animated, { FadeInLeft, FadeOutRight } from "react-native-reanimated";
+import { useForm, type DeepKeys } from "@tanstack/react-form";
+import * as z from "zod";
+import { ButtonSpinner } from "@/components/ui/button";
 
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
+const StrongPasswordSchema = z
+	.string()
+	.min(8, "At least 8 characters long")
+	.regex(/[a-z]/, "At least one lowercase letter")
+	.regex(/[A-Z]/, "At least one uppercase letter")
+	.regex(/\d/, "At least one number")
+	.regex(/[^a-zA-Z0-9\s]/, "At least one special character");
+
+const fieldsSchema = {
+	name: z.string().min(1, {
+		error: "Name is required",
+	}),
+	email: z.email(),
+	password: StrongPasswordSchema,
+	confirmPassword: z.string(), // Passwords match validation will be done at field level
+} satisfies z.ZodRawShape;
+
+type EmailSignupForm = z.infer<z.ZodObject<typeof fieldsSchema>>;
+
+const fieldsBuilder = [
+	{
+		name: "name",
+		label: "Name",
+		placeholder: "John Doe",
+	},
+	{
+		name: "email",
+		label: "Email",
+		placeholder: "hello@example.com",
+	},
+	{
+		name: "password",
+		label: "Password",
+		placeholder: "••••••••",
+	},
+	{
+		name: "confirmPassword",
+		label: "Confirm Password",
+		placeholder: "••••••••",
+	},
+] satisfies {
+	name: DeepKeys<EmailSignupForm>;
+	label: string;
+	placeholder: string;
+}[];
+
 export default function EmailSignup() {
-	// TODO: Integrate with Tanstack form
+	const form = useForm({
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+		} as EmailSignupForm,
+	});
+
 	return (
 		<AnimatedBox
 			className="flex-1 gap-12"
@@ -38,72 +95,109 @@ export default function EmailSignup() {
 				contentContainerClassName="gap-8"
 				showsVerticalScrollIndicator={false}>
 				<Box className="flex-1">
-					{(
-						[
-							{
-								path: "name",
-								label: "Name",
-								placeholder: "John Doe",
-								errorMessage:
-									"At least 2 characters are required.",
-							},
-							{
-								path: "email",
-								label: "Email",
-								placeholder: "hello@example.com",
-								errorMessage:
-									"At least 6 characters are required.",
-							},
-							{
-								path: "password",
-								label: "Password",
-								placeholder: "••••••••",
-								errorMessage:
-									"At least 8 characters are required.",
-							},
-						] satisfies {
-							path: string;
-							label: string;
-							placeholder: string;
-							errorMessage: string;
-						}[]
-					).map(({ path, label, placeholder, errorMessage }) => (
-						<FormControl
-							key={path}
-							isInvalid={false}
-							isDisabled={false}
-							isReadOnly={false}
-							isRequired={false}>
-							<FormControlLabel>
-								<FormControlLabelText>
-									{label}
-								</FormControlLabelText>
-							</FormControlLabel>
-							<Input className="h-16 rounded-2xl border-0 bg-background-50">
-								<InputField
-									placeholder={placeholder}
-									className="font-semibold"
-								/>
-							</Input>
+					{fieldsBuilder.map(({ name, label, placeholder }) => {
+						return (
+							<form.Field
+								key={name}
+								name={name}
+								validators={{
+									onBlurListenTo:
+										name === "confirmPassword"
+											? ["password"]
+											: undefined,
+									onBlur: ({ value, fieldApi }) => {
+										const fieldValidator =
+											name === "confirmPassword"
+												? fieldsSchema[name].refine(
+														(data) =>
+															data ===
+															fieldApi.form.getFieldValue(
+																"password",
+															),
+														{
+															error: "Passwords do not match",
+														},
+													)
+												: fieldsSchema[name];
 
-							<FormControlError>
-								<FormControlErrorIcon as={AlertCircleIcon} />
-								<FormControlErrorText>
-									{errorMessage}
-								</FormControlErrorText>
-							</FormControlError>
-						</FormControl>
-					))}
+										const validationResult =
+											fieldValidator.safeParse(value);
+
+										if (!validationResult.success) {
+											return validationResult.error
+												.issues;
+										}
+									},
+								}}>
+								{(field) => (
+									<FormControl
+										isInvalid={!field.state.meta.isValid}
+										isRequired={
+											!fieldsSchema[name].safeParse(
+												undefined,
+											).success
+										}>
+										<FormControlLabel>
+											<FormControlLabelText>
+												{label}
+											</FormControlLabelText>
+										</FormControlLabel>
+										<Input className="h-16 rounded-2xl border-0 bg-background-50">
+											<InputField
+												placeholder={placeholder}
+												className="font-semibold"
+												value={field.state.value}
+												onBlur={field.handleBlur} // for validate on blur
+												onChangeText={
+													field.handleChange
+												} // for syncing changes to field state
+											/>
+										</Input>
+
+										<FormControlError>
+											<FormControlErrorIcon
+												as={AlertCircleIcon}
+											/>
+											<FormControlErrorText>
+												{
+													// display one error at a time
+													field.state.meta.errors[0]
+														?.message
+												}
+											</FormControlErrorText>
+										</FormControlError>
+									</FormControl>
+								)}
+							</form.Field>
+						);
+					})}
 				</Box>
 
-				<FlowButton
-					accessibilityLabel={"Sign up"}
-					isDisabled={false}
-					className="mx-auto">
-					<FlowButtonRing>
-						<FlowButtonIcon as={ChevronRight} />
-					</FlowButtonRing>
-				</FlowButton>
+				<form.Subscribe
+					selector={({ canSubmit, isSubmitting }) => ({
+						canSubmit,
+						isSubmitting,
+					})}>
+					{({ canSubmit, isSubmitting }) => {
+						const isDisabled = !canSubmit || isSubmitting;
+						return (
+							<FlowButton
+								accessibilityLabel={"Sign up"}
+								accessibilityState={{ disabled: isDisabled }}
+								isDisabled={isDisabled}
+								onPress={() => void form.handleSubmit()}
+								className="mx-auto">
+								<FlowButtonRing>
+									{isSubmitting ? (
+										<ButtonSpinner />
+									) : (
+										<FlowButtonIcon as={ChevronRight} />
+									)}
+								</FlowButtonRing>
+							</FlowButton>
+						);
+					}}
+				</form.Subscribe>
 			</ScrollView>
 		</AnimatedBox>
 	);
