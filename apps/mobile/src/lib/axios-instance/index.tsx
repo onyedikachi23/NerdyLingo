3a602f85a1b4c41f1fc54d50a9ccc26c1032c874
onyedkichi23 <ozohenrywk@gmail.com>
2025-09-07 18:19:49 +0100

    feat(mobile): Add error types and guard for axios api errors.

--- apps/mobile/src/lib/axios-instance/index.tsx
+++ apps/mobile/src/lib/axios-instance/index.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import axios from "axios";
+import axios, { AxiosError, isAxiosError } from "axios";
 import z from "zod";
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
@@ -8,7 +8,7 @@ const baseURLValidation = z.url().safeParse(process.env.EXPO_PUBLIC_API_URL);
 if (!baseURLValidation.success) {
 	console.error(
 		"[axios instance]: EXPO_PUBLIC_API_URL env is invalid",
-		baseURLValidation.error,
+		baseURLValidation.error.issues,
 	);
 	throw new Error("[axios instance]: EXPO_PUBLIC_API_URL env is invalid");
 }
@@ -17,6 +17,33 @@ const apiAxiosInstance = axios.create({
 	baseURL: baseURLValidation.data,
 });
 
+interface ApiErrorResponse {
+	statusCode: number;
+	/**Human readable description of the error cause. */
+	message: string;
+	/**Short name of the error based on status code. */
+	error: string;
+}
+
+type AxiosApiError<T extends ApiErrorResponse = ApiErrorResponse> = RequireKeys<
+	AxiosError<T>,
+	"response"
+>;
+
+const isAxiosApiError = (error: unknown): error is AxiosApiError => {
+	if (!isAxiosError(error) || !error.response) {
+		return false;
+	}
+	const data: unknown = error.response.data;
+	return (
+		!!data &&
+		typeof data === "object" &&
+		"message" in data &&
+		typeof data.message === "string"
+	);
+};
+
 // TODO: Add interceptors
 
-export { apiAxiosInstance };
+export { apiAxiosInstance, isAxiosApiError };
+export type { ApiErrorResponse, AxiosApiError };
