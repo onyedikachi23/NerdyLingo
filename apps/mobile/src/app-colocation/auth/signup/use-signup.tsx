50787ebd5d09cb2b68d345ef1d1791b4b2eadb26
onyedkichi23 <ozohenrywk@gmail.com>
2025-09-08 17:04:37 +0100

    feat(mobile): add secure store and persist auth token o signup

--- apps/mobile/src/app-colocation/auth/signup/use-signup.tsx
+++ apps/mobile/src/app-colocation/auth/signup/use-signup.tsx
@@ -5,20 +5,45 @@ import { apiAxiosInstance } from "@/lib/axios-instance";
 import { getErrorMessage } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import type { EmailSignupForm } from "./types";
+import * as SecureStore from "expo-secure-store";
+
+interface User {
+	id: number;
+	name: string;
+	email: string;
+	password: string;
+}
+
+interface AuthSignupResponse {
+	accessToken: string;
+	user: SafeOmit<User, "password">;
+}
+
+const ACCESS_TOKEN_KEY = "auth-access-token";
 
 export const useSignup = () => {
 	return useMutation({
 		mutationFn: async (body: EmailSignupForm) => {
-			const response = await apiAxiosInstance.post<object>(
+			const response = await apiAxiosInstance.post<AuthSignupResponse>(
 				"/auth/signup",
 				body,
 			);
 			return response.data;
 		},
-		onSuccess: (data) => {
-			toast.success("Signup successful", {
-				description: JSON.stringify(data, null, 2),
-			});
+		onSuccess: async (data) => {
+			try {
+				await SecureStore.setItemAsync(
+					ACCESS_TOKEN_KEY,
+					data.accessToken,
+				);
+
+				// TODO: Save data.user directly to query cache for profile
+				toast.success("Signup successful");
+			} catch (e) {
+				toast.error("Failed to save login information.", {
+					description: getErrorMessage(e),
+				});
+			}
 		},
 		onError: (e) => {
 			toast.error("Signup failed", {
